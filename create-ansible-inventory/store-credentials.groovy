dd71a2870ed684693f159cee4029d4b191cb4c99
Tomonori Matsumura <matsumura_t@yahoo.co.jp>
2020-05-24 12:46:10 +0000

    「create-ansible-inventory」でAnsible inventoryファイルをJenkinsのCredentialsへ登録するように変更した refs #1180

--- create-ansible-inventory/store-credentials.groovy
+++ create-ansible-inventory/store-credentials.groovy
@@ -1,9 +1,17 @@
 def upload() {
   import jenkins.model.*
   import com.cloudbees.plugins.credentials.*
-  import com.cloudbees.plugins.credentials.domains.Domain
-  import org.jenkinsci.plugins.plaincredentials.impl.FileCredentialsImpl
-  import java.nio.file.*
+  import com.cloudbees.plugins.credentials.common.*
+  import com.cloudbees.plugins.credentials.domains.*
+  import com.cloudbees.plugins.credentials.impl.*
+  import com.cloudbees.jenkins.plugins.sshcredentials.impl.*
+  import org.jenkinsci.plugins.plaincredentials.*
+  import org.jenkinsci.plugins.plaincredentials.impl.*
+  import hudson.util.Secret
+  import hudson.plugins.sshslaves.*
+  import org.apache.commons.fileupload.* 
+  import org.apache.commons.fileupload.disk.*
+  import java.nio.file.Files
   
   Path fileLocation = Paths.get("${env.ANSIBLE_INVENTORY_DIR}/${params.INVENTORY_FILE_NAME}")
   
